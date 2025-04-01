1924a7b165ce659b46209fcb41692633f791d77a
kafou135 <youssefkafou@gmail.com>
2025-04-01 14:08:37 +0000

    Removed all files

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingComponent from "./components/LoadingComponent";
-
-export default function Loading() {
-    return (
-        <div className="flex flex-col w-full justify-center items-center h-screen">
-            <LoadingComponent color="#22bb11" />
-        </div>
-    )
-}
