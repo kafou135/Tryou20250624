79f591e3aba213942b011afc6996e8b7d507dd21
kafou135 <youssefkafou@gmail.com>
2025-04-14 00:50:04 +0100

    Update website

--- app/team/[id]/components/Fixtures.tsx
+++ app/team/[id]/components/Fixtures.tsx
@@ -19,9 +19,15 @@ export default function Fixtures({ fixturesByTeamId, teamId }: PageProps) {
     const handleShowMore = () => setVisibleItemsCount((prevCount) => prevCount + 5);
 
     const today = moment().format("YYYY-MM-DD");
-    const fixturesDone = fixturesByTeamId.filter(fixture => moment(fixture.fixture.date).format("YYYY-MM-DD") < today).reverse();
-    const fixturesToday = fixturesByTeamId.filter(fixture => moment(fixture.fixture.date).format("YYYY-MM-DD") === today);
-    const fixturesFuture = fixturesByTeamId.filter(fixture => moment(fixture.fixture.date).format("YYYY-MM-DD") > today);
+    const allFixtures = Object.values(fixturesByTeamId).flat();
+
+    const fixturesDone = allFixtures
+    .filter(fixture => fixture?.fixture?.date && moment(fixture.fixture.date).format("YYYY-MM-DD") < today)
+    .reverse();
+      const fixturesToday = allFixtures
+    .filter(fixture => fixture?.fixture?.date && moment(fixture.fixture.date).format("YYYY-MM-DD") === today);
+    const fixturesFuture = allFixtures
+    .filter(fixture => fixture?.fixture?.date && moment(fixture.fixture.date).format("YYYY-MM-DD") > today);
     const firstItemsFixturesFuture = fixturesFuture.slice(0, 5);
     
     const prevItem = () => setCurrentIndex(prev => Math.max(prev - 1, 0));
--- app/util/getFixturesByTeamId.tsx
+++ app/util/getFixturesByTeamId.tsx
@@ -1,39 +1,36 @@
 import { Fixture } from '@/types';
 import 'server-only';
-import getFixtures from './getFixtures1';
-import moment from 'moment';
+import { Redis } from "@upstash/redis";
 
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL2!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN2!,
+});
 
-export default async function getFixturesByTeamId(teamid: number,teamName:string,season:number,leagueid:number): Promise<Fixture[]> {
-    try {
-        const allFixturesByLeague = await getFixtures(teamName,season,leagueid);
+export default async function getFixtureByTeamId(teamid: number,teamName:string,season:number,leagueid:number) {
+    const cacheKey = `fixtureByteamId:${teamid}`;
 
-        const fixturesByTeamId: Fixture[] = [];
-
-        for (const league of allFixturesByLeague) {
-            for (const fixture of league.fixtures) {
-                if (fixture.teams.home.id === teamid || fixture.teams.away.id === teamid) {
-                    fixturesByTeamId.push(fixture);
-                }
-            }
-        }
-
-        const fixturesByTeamIdSorted: Fixture[] = fixturesByTeamId.sort((a, b) => {
-            const time1 = moment(a.fixture.date);
-            const time2 = moment(b.fixture.date);
-
-            if (time1.isBefore(time2)) {
-                return -1;
-            } else if (time1.isAfter(time2)) {
-                return 1;
-            } else {
-                return 0;
-            }
-        });
-
-        return fixturesByTeamIdSorted;
-    } catch (error) {
-        console.error('An Error occurred while fetching fixtures by Team Id: ', error);
-        throw error;
+  // Try to get from Redis
+  const cached = await redis.get(cacheKey);
+  if (cached) {
+    return cached;
+  }
+    const response = await fetch(`https://v3.football.api-sports.io/fixtures?team=${teamid}&season=${season}`, {
+      method: 'GET',
+      headers: {
+        'x-apisports-key': 'bfc80e7adee96f66d9666e447c62298d',
+        'x-rapidapi-host': 'v3.football.api-sports.io', // optional, may not be required anymore
+      },
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Failed to fetch fixture with ID ${teamid}`);
     }
-}
+  
+    const data = await response.json();
+    const fixture = data.response; // or data.response for the array
+    await redis.set(cacheKey, fixture, { ex: 60 });
+
+  return fixture;
+  };
+  
