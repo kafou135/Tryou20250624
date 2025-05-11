0511ffbbeca2facf675a312d784888f87e59d1cf
kafou135 <youssefkafou@gmail.com>
2025-05-11 19:09:09 +0100

    Update website

--- app/util/getFixtures5.tsx
+++ app/util/getFixtures5.tsx
@@ -41,7 +41,7 @@ async function fetchFixturesByLeague(
     lastWeek: string,
     nextWeek: string
 ): Promise<Fixture[]> {
-const nextWeek1 = moment().subtract(1, 'days').format('YYYY-MM-DD');        const lastWeek1 = moment().subtract(7, 'days').format('YYYY-MM-DD');    const url = `https://v3.fball.api-sports.io/fixtures?league=${league}&season=${year + yearr}&from=${lastWeek1}&to=${nextWeek1}`;    const options = {
+const nextWeek1 = moment().subtract(1, 'days').format('YYYY-MM-DD');        const lastWeek1 = moment().subtract(7, 'days').format('YYYY-MM-DD');    const url = `https://v3.football.api-sports.io/fixtures?league=${league}&season=${year + yearr}&from=${lastWeek1}&to=${nextWeek1}`;    const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': API_KEY,
