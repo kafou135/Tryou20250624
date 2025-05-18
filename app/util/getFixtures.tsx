ff308d8b31d6d0fcffbc203fe58d817aeab4f03f
kafou135 <youssefkafou@gmail.com>
2025-05-18 23:14:44 +0100

    Update website

--- app/util/getFixtures.tsx
+++ app/util/getFixtures.tsx
@@ -4,18 +4,7 @@ import { Redis } from "@upstash/redis";
 
 const API_KEY = process.env.API_KEY as string;
 
-// Redis setup
-const REDIS_URL = process.env.UPSTASH_REDIS_REST_URL;
-const REDIS_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN;
 
-if (!REDIS_URL || !REDIS_TOKEN) {
-  throw new Error("🚨 Redis environment variables are missing.");
-}
-
-const redis = new Redis({
-  url: REDIS_URL,
-  token: REDIS_TOKEN,
-});
 
 const leagues =    [
     { league: 2, name: 'EPL' ,yearr:-1, startmonth: '2024-07-01', endmonth: '2025-06-01'},
