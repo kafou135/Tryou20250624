b49e6b2dd86a3bcde9bf4f343d3346fb42264eae
kafou135 <youssefkafou@gmail.com>
2025-05-20 13:32:15 +0100

    Update website

--- app/util/getStanding.tsx
+++ app/util/getStanding.tsx
@@ -3,19 +3,17 @@ import 'server-only';
 import { Standing } from "@/types";
 import moment from "moment";
 
-export default async function getStandings(yearr:number,id:number): Promise<Standing[][]> {
+export default async function getStandings(yearr:number,id:number): Promise<Standing[]> {
 
    
 
-    const currentTime = moment().format('YYYY-MM-DD')
-    const month = moment().month();
+    const currentTime = moment();
+    const month = currentTime.month();
     let year;
 
-    if (month <= 6) {
-        year = moment().year() - 1;
-    } else {
-        year = moment().year();
-    }
+    
+        year = currentTime.year() - 1;
+    
 
     const API_KEY: string = process.env.API_KEY as string;
 
@@ -24,12 +22,18 @@ export default async function getStandings(yearr:number,id:number): Promise<Stan
         headers: {
             'X-RapidAPI-Key': API_KEY,
         },
-        
+        next: {
+            revalidate: 60 * 60 * 24
+        }
     };
 
-    const standings: Standing[][] = [];
+    const standings: Standing[] = [];
 
-    
+    const leagues = [
+        { name: 'EPL', id: 39 },
+    ]
+
+    for (const league of leagues) {
         let url = `https://v3.football.api-sports.io/standings?season=${2024+yearr}&league=${id}`
 
         try {
@@ -41,9 +45,9 @@ export default async function getStandings(yearr:number,id:number): Promise<Stan
               standings.push(standing);
             }
         } catch (err) {
-            console.error(`Error fetching ${id} standings: ${err}`);
+            console.error(`Error fetching ${league.name} standings: ${err}`);
         }
-    
+    }
 
     return standings;
 }
