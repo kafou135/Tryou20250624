71c5b69ba24ca52652746ff35763a9f295846af0
kafou135 <youssefkafou@gmail.com>
2025-05-31 16:36:37 +0100

    Update website

--- app/team/[id]/page.tsx
+++ app/team/[id]/page.tsx
@@ -74,7 +74,8 @@ export default async function Team({
         {/* Optional: other Twitter/OpenGraph tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:description" content={`Explore ${teamName}'s latest stats, league form, and upcoming matches for the ${season}/${season + 1} season. Follow live scores and detailed insights on Gridiola.`} />
-      </head>        <div className="flex justify-center items-center text-neutral-100 py-5">
+      </head>        
+      <div className="flex justify-center items-center text-neutral-100 py-5">
             <div className="flex flex-col max-w-7xl p-5 w-full md:flex-row gap-5 bg-gray-900 rounded-lg shadow-lg">
                 <div className="flex flex-col md:w-1/3 justify-center items-center bg-gray-800 rounded-lg p-5 shadow-md">
                     <img
