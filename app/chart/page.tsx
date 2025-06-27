624db09609f6e066a168d9f37a7f6740ea06e10a
kafou135 <youssefkafou@gmail.com>
2025-06-27 14:49:04 +0100

    Update website

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -3,7 +3,6 @@ import BTC_MACD_Chart from "./components/BTC_MACD_Chart";
 import BTCIndic from "./components/indicators";
 import SignalPanel from "./components/SignalPanel";
 import SignalPanel1 from "./components/SignalPanel copy";
-import BTC_RSI_Chart from "./components/RSI_indicatot";
 import MACDScalpingSignal from "./components/MACD_SCALPING_SIGNAL";
 import MACDScalpingSignal1 from "./components/MACD_SCALPING_SIGNAL copy";
 export default function Home() {
